c0a2c3a361640bd995ada7b7299b944a7dd78ea0
Marc-07 <maanraca01@gmail.com>
2023-04-04 20:38:10 -0500

    Se agrega alert en el HTML

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const inputEmail = document.querySelector("#email");
     const inputAsunto = document.querySelector("#asunto");
     const inputMensaje = document.querySelector("#mensaje");
+    const formulario = document.querySelector("#formulario");
 
 
   //Asignar Evento
@@ -23,8 +24,10 @@ document.addEventListener("DOMContentLoaded", function(){
     function mostrarAlerta(){
         //Generar alerta en HTML
         const error = document.createElement("P");
-        error.textContent = "Hubo un error..."
-        
-        console.log(error)
+        error.textContent = "Hubo un error...";
+        error.classList.add("bg-red-600", "text-white", "p-2", "text-center")
+
+        //Inyectar el error al formulario
+        formulario.appendChild(error)
     }
 });
