de12553ab297012570ffc77ed7ee1079c4d7b775
Marc-07 <maanraca01@gmail.com>
2023-04-04 19:42:49 -0500

    Se asignan eventos

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
-//Seleccionar los elementos de la interfaz
- const inputEmail = document.querySelector("#email");
- const inputAsunto = document.querySelector("#asunto");
- const inputMensaje = document.querySelector("#mensaje");
+
+   //Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector("#email");
+    const inputAsunto = document.querySelector("#asunto");
+    const inputMensaje = document.querySelector("#mensaje");
 
 
- console.log(inputMensaje)
- 
+  //Asignar Evento
+
+    inputEmail.addEventListener("blur", validar);
+    inputAsunto.addEventListener("blur", validar);
+    inputMensaje.addEventListener("blur", validar);
+
+    function validar(e){
+        console.log(e.target.value)
+
+    }
 });
